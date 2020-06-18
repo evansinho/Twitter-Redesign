7b1ddbcb6601c7e9b8795459ce1ebcb2f4839dee
evansinho <igiri.evanson@gmail.com>
2020-06-18 19:12:23 +0100

    finish setting up cloudinary and added columns to the users table

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
   before_save { self.Username = self.Username.downcase }
   validates :Username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3 }
   validates :Fullname, presence: true, length: { minimum: 3, maximum: 100 }
+  mount_uploader :Photo, ImageUploader
+  mount_uploader :CoverImage, ImageUploader
 
   def self.user_followers(id, curr_user_id)
     Following.where(Followedid: id).where.not(Followerid: curr_user_id).order(created_at: :desc).limit(5)
