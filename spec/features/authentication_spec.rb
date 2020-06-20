94c60c30ee9016cecb6d64101ef20091b9c0c1d6
evansinho <igiri.evanson@gmail.com>
2020-06-20 12:09:48 +0100

    write test for aunthentication feature

--- spec/features/authentication_spec.rb
+++ spec/features/authentication_spec.rb
@@ -6,7 +6,7 @@ RSpec.feature 'Authentications', type: :feature do
   end
 
   it 'Should Signup' do
-    visit new_user_path
+    visit signup_path
     fill_in 'Fullname', with: 'new_user'
     fill_in 'Username', with: 'newuser1'
     click_button 'Sign Up'
@@ -15,20 +15,18 @@ RSpec.feature 'Authentications', type: :feature do
 
   it 'Should Login' do
     visit root_path
-    fill_in 'Username', with: 'newuser1'
+    fill_in 'Username', with: 'user1'
     click_button 'Log In'
     expect(page).to have_content('HOME')
     expect(page).to have_content('PROFILE')
-    expect(page).to have_content('LOGOUT')
   end
 
   it 'Should Logout' do
     visit root_path
-    fill_in 'Username', with: 'newuser1'
+    fill_in 'Username', with: 'user1'
     click_button 'Log In'
     expect(page).to have_content('HOME')
     expect(page).to have_content('PROFILE')
-    expect(page).to have_content('LOGOUT')
     click_on 'LOGOUT'
     expect(page).to have_content('Log in to Twitter')
   end
